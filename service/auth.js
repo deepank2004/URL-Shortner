356622a1e15775dbcdad0ec5db35c397849bfefc
Deepank Sharma <gaurdeepank0@gmail.com>
2025-08-30 12:14:47 +0530

    JWT Authentication

--- service/auth.js
+++ service/auth.js
@@ -1,13 +1,25 @@
-const { get } = require("mongoose");
+//const { get } = require("mongoose");
+const jwt = require("jsonwebtoken")
+const secret = "Deep@2004"
 
 const sessionIdToUserMap = new Map();
 
-function setUser(id , user){
-    sessionIdToUserMap.set(id , user);
+function setUser(user){
+    //sessionIdToUserMap.set(id , user);
+    const payload = {
+        _id: user._id,
+        email: user.email
+    }
+    return jwt.sign(payload , secret);
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token){
+    if(!token) return null;
+    try {
+        return jwt.verify(token , secret)
+    } catch (error) {
+        return null;
+    }
 }
 
 module.exports = {
